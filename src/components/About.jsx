420c0bb3cebdc117468b533299ae9b9348387c1d
Mazen <github.com/MazenME>
2025-05-10 19:24:54 +0300

    Final

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,18 +5,18 @@ export default function About() {
   return (
     <div className="about-page" style={{ padding: "2rem" }}>
       <Helmet>
-        <title>The Prophecy Of Shadows - Mazen Emad</title>
+        <title>Unseen Tone - Mazen Emad</title>
         <meta name="author" content="Mazen Emad Ramadan" />
         <meta
           name="description"
-          content="Read The Prophecy Of Shadows chapters written by Mazen Emad Ramadan."
+          content="Read Unseen Tone chapters written by Mazen Emad Ramadan."
         />
-        <meta property="og:title" content="The Prophecy Of Shadows - by Mazen" />
+        <meta property="og:title" content="Unseen Tone - by Mazen" />
       </Helmet>
 
       <h1>About This Project</h1>
       <p>
-        This website was built by <strong>Mazen Emad Ramadan </strong> the author of <strong>The Prophecy Of Shadows Novel</strong>, a
+        This website was built by <strong>Mazen Emad Ramadan </strong> the author of <strong>Unseen Tone Novel</strong>, a
         passionate front-end developer who loves building practical web apps for
         real users.
       </p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <Link to="/">The Prophecy of Shadows</Link>
+        <Link to="/">Unseen Tone</Link>
       </div>
       <ul className="navbar-links">
         <li><Link to="/">Chapters</Link></li>
